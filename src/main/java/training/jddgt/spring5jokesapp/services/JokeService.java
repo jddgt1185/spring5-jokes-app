c295b2f083a020c66f5388cff7dde93be345a666
juandediosgarcia <juandediosgarciatrejo@gmail.com>
2023-08-14 18:05:13 -0600

    Commiting last changes

---
+++ src/main/java/training/jddgt/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package training.jddgt.spring5jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/training/jddgt/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package training.jddgt.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes =  new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
